a3488f12887be8a270b0cf28b619f845bed1a551
gandzja <gandzja@gmail.com>
2019-09-20 18:09:52 +0300

    portfolio 1

---
+++ public/views/diplom_portfolio/js/common.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.cssload-loader-inner').find('i').fadeOut().end().delay(400).fadeOut('slow');
+});
+
+$( document ).ready(function() {
+    $('.cssload-loader-inner').addClass("HIDE");
+});
