12046619769b93d491c007a26d3e5a3b371f9999
gandzja <gandzja@gmail.com>
2019-10-09 09:38:35 +0300

    calc

--- public/views/diplom_portfolio/js/common.js
+++ public/views/diplom_portfolio/js/common.js
@@ -1,10 +1,4 @@
-// $(function() {
-//     $('.cssload-loader-inner').find('i').fadeOut().end().delay(400).fadeOut('slow');
-// });
 
-// $( document ).ready(function() {
-//     $('.cssload-loader-inner').addClass("HIDE");
-// });
 !function(a){
   a.fn.preloadinator=function(e){
     "use strict";
@@ -21,11 +15,9 @@
       }
     },e),n=this,r=(new Date).getTime();
     return a.fn.preloadinator.disableScroll=function(){
-      a('.wrapper').css("opacity", "0")
-      a("body").css("overflow","hidden"),"function"==typeof o.afterDisableScroll&&o.afterDisableScroll.call(this)
+      a('.wrapper').css("opacity", "0"),"function"==typeof o.afterDisableScroll&&o.afterDisableScroll.call(this)
     },a.fn.preloadinator.enableScroll=function(){
-      a('.wrapper').css("opacity", "1")
-      a("body").css("overflow","auto"),"function"==typeof o.afterEnableScroll&&o.afterEnableScroll.call(this)
+      a('.wrapper').css("opacity", "1"),"function"==typeof o.afterEnableScroll&&o.afterEnableScroll.call(this)
     },a.fn.preloadinator.removePreloader=function(){
       a(n)[o.animation](o.animationDuration,function(){!1===o.scroll&&a.fn.preloadinator.enableScroll(),"function"==typeof o.afterRemovePreloader&&o.afterRemovePreloader.call(this)})
     },a.fn.preloadinator.minTimeElapsed=function(){
@@ -44,9 +36,61 @@
 $(function() {
   $('.wrap-card').click(function() {
       $('.wrap-card').each(function() {
-         $(this).css('z-index', 0); 
+        $(this).css('z-index', 0);
       });
-      	$(this).css('z-index', 10); 
-      	$(this).toggleClass('open');    
+        $(this).css('z-index', 10);
+        $(this).toggleClass('open');
   }) 
 })
+
+$(function () { // wait for document ready
+  var flightpath = {
+    entry : {
+      curviness: 1.25,
+      autoRotate: true,
+      values: [
+          {x: 100,	y: -20},
+          {x: 300,	y: 10}
+        ]
+    },
+    looping : {
+      curviness: 1.25,
+      autoRotate: true,
+      values: [
+          {x: 510,	y: 60},
+          {x: 620,	y: -60},
+          {x: 500,	y: -100},
+          {x: 380,	y: 20},
+          {x: 500,	y: 60},
+          {x: 580,	y: 20},
+          {x: 620,	y: 15}
+        ]
+    },
+    leave : {
+      curviness: 1.25,
+      autoRotate: true,
+      values: [
+          {x: 660,	y: 20},
+          {x: 800,	y: 130},
+          {x: $(window).width() + 300,	y: -100},
+        ]
+    }
+  };
+  // init controller
+  var controller = new ScrollMagic.Controller();
+
+  // create tween
+  var tween = new TimelineMax()
+    .add(TweenMax.to($("#plane"), 1.2, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}))
+    .add(TweenMax.to($("#plane"), 2, {css:{bezier:flightpath.looping}, ease:Power1.easeInOut}))
+    .add(TweenMax.to($("#plane"), 1, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}));
+
+  // build scene
+  var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 500, offset: 100})
+          .setPin("#target")
+          .setTween(tween)
+          .addIndicators() // add indicators (requires plugin)
+          .addTo(controller);
+})
+
+
